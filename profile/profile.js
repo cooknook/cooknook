32503db46205aec6367800e584e47444a39253d9
Jerry Wang <68243688+thejerryw@users.noreply.github.com>
2024-08-25 13:53:18 -0700

    Add files via upload

---
+++ profile/profile.js
@@ -0,0 +1,20 @@
+function goRecipes() 
+{
+  window.location.href = "/recipes/recipes.html";
+  location.replace("/recipes/recipes.html");
+}
+
+function goProfile()
+{
+  window.location.href = "/profile/profile.html";
+  location.replace("/profile/profile.html");
+}
+
+function goHomepage() 
+{
+  window.location.href = "/homepage/homepage.html";
+  location.replace("/homepage/homepage.html");
+}
+
+
+
